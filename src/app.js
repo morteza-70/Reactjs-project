053bd38c49755ff149daeac3a447d3c33d214bda
morteza-70 <morteza.torkashvand70@gmail.com>
2022-01-10 16:36:36 +0330

    add app and componetns

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+
+// class Component example
+class App extends Component {
+    render () {
+        return (
+            <>
+                <h1>hello Morteza</h1>
+                <h1>this is react morteza</h1>
+            </>
+        )
+    }
+}
+
+export default App;
---
+++ src/components/product.jsx
@@ -0,0 +1,18 @@
+import { Component } from "react";
+
+class Product extends Component {
+    render() {
+        return (
+            <>
+                <span className="m-2 text-info">Product Name</span>
+                <span className="m-2 badge bg-primary">2</span>
+                <button className="m-2 btn btn-sm btn-success">+</button>
+                <button className="m-2 btn btn-sm btn-warning">-</button>
+                <button className="m-2 btn btn-sm btn-danger">delete</button>
+            </>
+        )
+    }
+}
+
+export default Product;
+
