e759b87de4e588308eb260a959fb92c74056f2d0
morteza-70 <morteza.torkashvand70@gmail.com>
2022-01-12 12:10:01 +0330

    use direct condition command with Conditional (ternary) operator(The second method)

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -2,18 +2,27 @@ import { Component } from "react";
 
 const list = ['item one', 'item two', 'item three']
 class Product extends Component {
-    count = 0;
+    count = 5;
     render() {
         return (
-            <>
-                <span className="m-2 text-info">Product Name</span>
-                <span className="m-2 badge bg-primary">{this.count === 0 ? 'zero' : this.count}</span>
-                <button className="m-2 btn btn-sm btn-success">+</button>
-                <button className="m-2 btn btn-sm btn-warning">-</button>
-                <button className="m-2 btn btn-sm btn-danger">delete</button>
-                <ul>
-                    {list.map((element, index) => <li key={index}>{element}</li>)}
-                </ul>
+            <> 
+                {
+                    this.count !== 5 ? (
+                        <>
+                            <span className="m-2 text-info">Product Name</span>
+                            <span className="m-2 badge bg-primary">{this.count === 0 ? "zero" : this.count}</span>
+                            <button className="m-2 btn btn-sm btn-success">+</button>
+                            <button className="m-2 btn btn-sm btn-warning">-</button>
+                            <button className="m-2 btn btn-sm btn-danger">delete</button>
+                            <ul>
+                                {list.map((element, index) => <li key={index}>{element}</li>)}
+                            </ul>
+                        </>
+                    )
+                    : (
+                        <h1>Product not available</h1>
+                    )
+                }
             </>
         )
     }
