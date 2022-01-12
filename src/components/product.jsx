43d76d930e218b6c701469e85e023eb835f8f9f9
morteza-70 <morteza.torkashvand70@gmail.com>
2022-01-12 12:39:17 +0330

    use direct condition command(The third method)

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -2,27 +2,18 @@ import { Component } from "react";
 
 const list = ['item one', 'item two', 'item three']
 class Product extends Component {
-    count = 5;
+    count = 0;
     render() {
         return (
-            <> 
-                {
-                    this.count !== 5 ? (
-                        <>
-                            <span className="m-2 text-info">Product Name</span>
-                            <span className="m-2 badge bg-primary">{this.count === 0 ? "zero" : this.count}</span>
-                            <button className="m-2 btn btn-sm btn-success">+</button>
-                            <button className="m-2 btn btn-sm btn-warning">-</button>
-                            <button className="m-2 btn btn-sm btn-danger">delete</button>
-                            <ul>
-                                {list.map((element, index) => <li key={index}>{element}</li>)}
-                            </ul>
-                        </>
-                    )
-                    : (
-                        <h1>Product not available</h1>
-                    )
-                }
+            <>
+                <span className="m-2 text-info">Product Name</span>
+                <span className="m-2 badge bg-primary">{this.count === 0 ? 'zero' : this.count}</span>
+                <button className="m-2 btn btn-sm btn-success">+</button>
+                <button className="m-2 btn btn-sm btn-warning">-</button>
+                <button className="m-2 btn btn-sm btn-danger">delete</button>
+                <ul>
+                    {list.map((element, index) => <li key={index}>{element}</li>)}
+                </ul>
             </>
         )
     }
