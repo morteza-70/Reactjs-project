7015cae16939f99fb6db2ffd71e7c118ea48c488
morteza-70 <morteza.torkashvand70@gmail.com>
2022-01-12 18:53:42 +0330

    3 type onclick  event in jsx

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -1,21 +1,37 @@
 import { Component } from "react";
+import './product.css'
 
-const list = ['item one', 'item two', 'item three']
+const list = ['item one', 'item two', 'item three'];
 class Product extends Component {
     count = 0;
+    imgSrc = "https:/picsum.photos/200";
     render() {
         return (
             <>
                 <span className="m-2 text-info">Product Name</span>
-                <span className="m-2 badge bg-primary">{this.count === 0 ? 'zero' : this.count}</span>
-                <button className="m-2 btn btn-sm btn-success">+</button>
-                <button className="m-2 btn btn-sm btn-warning">-</button>
-                <button className="m-2 btn btn-sm btn-danger">delete</button>
+                <span className="m-2 badge bg-primary">{this.format()}</span>
+                <button onClick={this.increament()} className="m-2 btn btn-sm btn-success">+</button>
+                <button onClick={this.decreament.bind()} className="m-2 btn btn-sm btn-warning">-</button>
+                <button onClick={() => console.log('delete')} className="m-2 btn btn-sm btn-danger">delete</button>
                 <ul>
                     {list.map((element, index) => <li key={index}>{element}</li>)}
                 </ul>
+                <img src="https:/picsum.photos/200" style={{borderRadius :'20%'}}></img>
+                <img src= {this.imgSrc}></img>
             </>
         )
+    }
+    format() {
+        if (this.count === 0)
+            return 'zero';
+        else
+            return this.count;
+    }
+    increament = () => {
+        console.log('increament' + a, this);
+    }
+    decreament(a) {
+        console.log('decreament');
     }
 }
 
