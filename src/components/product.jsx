9ab415c69a0880a552b1081aa207d9a38e91b521
morteza-70 <morteza.torkashvand70@gmail.com>
2022-01-29 13:05:54 +0330

    use state in react app

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -2,13 +2,15 @@ import { Component } from "react";
 import './product.css'
 
 const list = ['item one', 'item two', 'item three'];
-class Product extends Component {
-    count = 0;
+class Product extends Component {    
+    state = {  // when use that change value
+        count : 0
+    }
     imgSrc = "https:/picsum.photos/200";
     render() {
         return (
             <>
-                <span className="m-2 text-info">Product Name</span>
+                <span className="m-2 text-info">Laptop</span>   {/* this is out of state beacuse static value */}
                 <span className="m-2 badge bg-primary">{this.format()}</span>
                 <button onClick={this.increament} className="m-2 btn btn-sm btn-success">+</button>
                 <button onClick={this.decreament} className="m-2 btn btn-sm btn-warning">-</button>
@@ -22,17 +24,19 @@ class Product extends Component {
         )
     }
     format() {
-        if (this.count === 0)
+        if (this.state.count === 0)
             return 'zero';
         else
-            return this.count;
+            return this.state.count;
     };
 
     increament = ()=> {
-        console.log('increament');
+        const {count} = this.state;
+        this.setState({count: count + 1});
     }
     decreament = ()=> {
-        console.log('decreament');
+        const {count} = this.state;
+        this.setState({count: count - 1});
     }
     delete = () => {
         console.log('delete')
